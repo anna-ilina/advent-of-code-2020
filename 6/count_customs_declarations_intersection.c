2903118711e7dab80214ae7626183537bd441770
anna-ilina <25167864+anna-ilina@users.noreply.github.com>
2020-12-06 12:16:10 -0500

    Solve day 6

---
+++ 6/count_customs_declarations_intersection.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <errno.h>
+
+#define NUM_QUESTIONS 26
+
+
+int blankLineNext(FILE *stream) {
+    int c1, c2, c3;
+
+    c1 = fgetc(stream);
+    c2 = fgetc(stream);
+    c3 = fgetc(stream);
+    ungetc(c3, stream);
+    ungetc(c2, stream);
+    ungetc(c1, stream);
+
+    return (c1 == EOF || c1 == '\r' && c2 == '\n' && c3 == '\r');
+}
+
+void resetQuestions(char *questions){
+    for (int i=0; i < NUM_QUESTIONS; i++){
+        questions[i] = 'O';
+    }
+    questions[NUM_QUESTIONS] = '\0';
+}
+
+void markAnsweredQuestions(char *input, char *groupQuestions, int groupMemberCount){
+
+    // For first group member, mark groupQuestions based on their input
+    if (groupMemberCount == 0){
+        int len = strlen(input);
+        for (int i=0; i<len; i++){
+            groupQuestions[input[i] - 'a'] = 'X';
+        }
+        printf("Member %d's input: %s\nGroup questions: %s\n", groupMemberCount, input, groupQuestions);
+        return;
+    }
+
+    // For non-first group member, unmark any groupQuestions
+    // that the member didn't select
+    // (All members of a group must mark a question for
+    // it to count towards the group)
+
+    char individualQuestions[NUM_QUESTIONS + 1];
+    resetQuestions(individualQuestions);
+
+    int len = strlen(input);
+    for (int i=0; i<len; i++){
+        individualQuestions[input[i] - 'a'] = 'X';
+    }
+
+    printf("Member %d's input: %s\nIndividual questions: %s\n", groupMemberCount, input, individualQuestions);
+
+    printf("Group questions before comparison: %s\n", groupQuestions);
+
+    for (int i=0; i < NUM_QUESTIONS; i++){
+        if (groupQuestions[i] == 'X' && individualQuestions[i] != 'X'){
+            groupQuestions[i] = '0';
+        }
+    }
+
+    printf("Group questions after comparison: %s\n", groupQuestions);
+
+}
+
+
+int countAnsweredQuestions(char *questions){
+    int count = 0;
+    for (int i=0; i < NUM_QUESTIONS; i++){
+        if (questions[i] == 'X') {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int main(int argc, char *argv[]) {
+    FILE *file;
+    // argv[1] is filename
+    file = fopen(argv[1], "r");
+    if (!file) {
+        // alternatively, just `perror(argv[1])`
+        fprintf(stderr, "cannot open %s: %s\n", argv[1], strerror(errno));
+        return 1;
+    }
+
+    // Create array of chars to keep track of answered questions
+    char questions[NUM_QUESTIONS + 1];
+    resetQuestions(questions);
+
+    // Read passports from file
+    int result = 0;
+    int groupMemberCount = 0;
+    char inputLine[NUM_QUESTIONS+1];
+    while (!feof (file)) {
+        fscanf(file, "%s", &inputLine[0]);
+
+        markAnsweredQuestions(inputLine, questions, groupMemberCount);
+
+        if (blankLineNext(file)){
+            int numAnsweredQuestions = countAnsweredQuestions(questions);
+            printf("End of group reached, answered Q's: %s (%d)\n\n", questions, numAnsweredQuestions);
+            result += numAnsweredQuestions;
+            resetQuestions(questions);
+            groupMemberCount = 0;
+        } else {
+            groupMemberCount++;
+        }
+
+    }
+    fclose(file);
+
+    printf("Result: %d\n", result);
+    return 0;
+}
